275e3e5b1f498011f588eb520003b2decddfa6dd
Jakob Korherr <jakob.korherr@gmail.com>
2015-09-19 15:16:48 +0200

    cleanup and javadoc

--- src/main/java/javax/persistence/async/AsyncEntityOperations.java
+++ src/main/java/javax/persistence/async/AsyncEntityOperations.java
@@ -1,19 +1,15 @@
 package javax.persistence.async;
 
-import java.util.concurrent.CompletableFuture;
-
 /**
- * TODO
+ * Interface combining (asynchronous) operations affecting entities.
  *
  * @author Jakob Korherr
  */
 public interface AsyncEntityOperations {
 
-    // TODO more operations
-
-    public void persist();
-
-    public CompletableFuture<Void> flush();
+    // TODO support more operations
+    //public void persist();
+    //public CompletableFuture<Void> flush();
 
     public <T> AsyncQuery<T> createQuery(String qlString, Class<T> resultClass);
 
--- src/main/java/javax/persistence/async/AsyncEntityTransaction.java
+++ src/main/java/javax/persistence/async/AsyncEntityTransaction.java
@@ -1,22 +1,14 @@
 package javax.persistence.async;
 
-import java.util.concurrent.CompletableFuture;
-
 /**
- * TODO
+ * Asynchronous version of {@link javax.persistence.EntityTransaction}.
  *
  * @author Jakob Korherr
  */
 public interface AsyncEntityTransaction extends AsyncEntityOperations {
 
-    public CompletableFuture<Void> commit();
-
-    public CompletableFuture<Void> rollback();
-
     public void setRollbackOnly();
-
     public boolean getRollbackOnly();
-
     public boolean isActive();
 
 }
